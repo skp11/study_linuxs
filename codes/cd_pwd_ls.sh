5f2c87bb667a162a10032bc1f21a5def11b45236
skp11 <87636089+skp11@users.noreply.github.com>
2025-07-14 18:02:17 +0900

    cd_pwd_ls.sh 만듦

---
+++ codes/cd_pwd_ls.sh
@@ -0,0 +1,12 @@
+# Music 폴더로 이동 후 내용 확인
+[skp@localhost ~]$ cd /home/skp/Music/
+[skp@localhost Music]$ pwd
+/home/skp/Music
+[skp@localhost Music]$ ls
+
+# Pictures 폴더로 이동 후 내용 확인
+[skp@localhost Music]$ cd /home/skp/Pictures/
+[skp@localhost Pictures]$ pwd
+/home/skp/Pictures
+[skp@localhost Pictures]$ ls
+[skp@localhost Pictures]$ 
