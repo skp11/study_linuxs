cec5b385ae3611e5f3d8dba681552f2fa24192b2
skp11 <87636089+skp11@users.noreply.github.com>
2025-07-15 12:23:08 +0900

    두 번째 올리기 (powershell 실습 & README.md 작성)

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # study_linuxs
+## powershell (GUI & CLI)
+연습 글을 올립니다.
+- 첫 번째
+- 두 번째
+
+[안내글](codes/10_powershells.sh)
---
+++ codes/10_powershells.sh
@@ -0,0 +1,35 @@
+# pwd 로 현재 위치 확인
+PS C:\Users\Administrator> pwd
+
+Path
+----
+C:\Users\Administrator
+
+
+# cd 로 C드라이브로 이동
+PS C:\Users\Administrator> cd C:\
+
+
+# cd 로 Develops로 이동
+PS C:\> cd C:\Develops
+
+
+# pwd 로 현재 위치 확인
+PS C:\Develops> pwd
+
+Path
+----
+C:\Develops
+
+
+# ls 로 Develops 폴더의 내용 확인
+PS C:\Develops> ls
+
+
+    디렉터리: C:\Develops
+
+
+Mode                 LastWriteTime         Length Name
+----                 -------------         ------ ----
+d-----      2025-07-14  오전 11:59                Downloads
+d-----      2025-07-15  오전 11:35                VMs
